7ceac4a317c05133063bf027fff8faa3985d6791
scardinius <scardinius@chords.pl>
2015-06-08 17:26:53 +0200

    Jasmine-jQuery working examples (structure of form)

--- civicrm/tests/karma/unit/webformPetition.js
+++ civicrm/tests/karma/unit/webformPetition.js
@@ -5,12 +5,8 @@ describe('Petition form', function(){
   jasmine.getFixtures().fixturesPath = '';
   var fixtureUrl = 'http://localhost:8001/node/3';  // form with petition
   
-  // form id fields for petition 
-  //'civicrm-1-contact-1-contact-first-name'
-  //'civicrm_1_contact_1_contact_last_name',
-  //'civicrm_1_contact_1_email_email',
-  //'civicrm_1_contact_1_address_country_id',
-  //'civicrm_1_contact_1_address_city',
+  var formId = 'webform-client-form-3';
+  var labelRequiredClass = 'form-required';
   var fieldsId = {
     'edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-first-name' : {'required' : true},
     'edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-contact-last-name' : {'required' : true},
@@ -18,34 +14,75 @@ describe('Petition form', function(){
     'edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-address-country-id' : {'required' : true},
     'edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-address-city' : {'required' : false}
   };
+  var hiddenNames = {
+    'details[sid]' : {},
+    'details[page_num]' : {},
+    'details[page_count]' : {},
+    'details[finished]' : {},
+    'form_build_id' : {},
+    'form_id' : {}
+  };
+  
   
   beforeEach(function(){
     jasmine.getFixtures().clearCache();
-    jasmine.getFixtures().load(fixtureUrl); 
+    jasmine.getFixtures().load(fixtureUrl);     
   });
   
       
   it("should has all fields", function () {
     var id;
-    for (id in fieldsId)
-    {
+    for (id in fieldsId) {
       var el = jQuery("#"+id);
       expect(el).toHaveId(id);
     }
-  });  
+  });
     
     
-  it("should has fields with attribute required", function () {
+  it("should has label elements for all fields", function () {
     var id;
-    for (id in fieldsId)
-    {
-      if (fieldsId[id].required)
-      {
+    for (id in fieldsId) {
+      var el = jQuery("label[for="+id+"]");
+      expect(el).toExist();
+    }
+  });
+    
+    
+  it("should has fields with attribute 'required'", function () {
+    var id;
+    for (id in fieldsId) {
+      if (fieldsId[id].required) {
         var el = jQuery("#"+id);
         expect(el).toHaveAttr('required');
-      }      
+      }
     }
   });  
   
+  
+  it("should has annotation about 'required' fields", function () {
+    var id;
+    for (id in fieldsId) {
+      if (fieldsId[id].required) {
+        var el = jQuery("label[for="+id+"] ."+labelRequiredClass);
+        expect(el).toExist();
+      }
+    }
+  }); 
+  
+  
+  it("should has submit button", function(){
+    var el = jQuery("form#"+formId+" input[type=submit]");
+    expect(el).toExist();
+    expect(el.length).toBe(1);
+  });
+  
+  
+  it("should has required hidden fields", function(){
+    var hiddenName;
+    for (hiddenName in hiddenNames) {
+      var el = jQuery("form#"+formId+" input[type=hidden][name='"+hiddenName+"']");
+      expect(el.length).toEqual(1); 
+    }
+  });
     
 });
